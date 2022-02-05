bb094eb45cf2186e13929430c79163d4e9696a64
EC2 Default User <ec2-user@ip-172-31-16-38.ap-southeast-1.compute.internal>
2022-02-05 11:37:15 +0000

    edited vi file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, guys");
 		System.out.println("Hello, guys");
-		System.out.println("Hello, guys");
+		System.out.println("End");
 	}
 }
